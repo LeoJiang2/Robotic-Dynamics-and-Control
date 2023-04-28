17416fd178e56f3ef76576b5b0a814fccd88bcc1
Leo Jiang <73407599+LeoJiang2@users.noreply.github.com>
2023-04-27 21:12:41 -0500

    lab4 commented

--- workspace/me446_labs_ljts/me446lab4_ljts.c
+++ workspace/me446_labs_ljts/me446lab4_ljts.c
@@ -47,10 +47,6 @@ float Simulink_PlotVar4 = 0;
 
 float thetaddot = 0;
 
-float J1 = 0.0167;
-float J2 = 0.03;
-float J3 = 0.0128;
-
 float a2 = 0;
 float a3 = 0;
 
@@ -64,6 +60,8 @@ float Omega1 = 0;
 float Omega2 = 0;
 float Omega3 = 0;
 
+
+//friction coefficients
 float Viscous_positive1 = 0.22;
 float Viscous_negative1 = 0.22;
 float Coulomb_positive1 = 0.3637;
@@ -87,12 +85,6 @@ float v_co_1 = 3.6;
 float v_co_2 = 3.6;
 float v_co_3 = 3.3;
 
-float p1 = 0.03;
-float p2 = 0.0128;
-float p3 = 0.0076;
-float p4 = 0.0753;
-float p5 = 0.0298;
-
 float at2 = 0.0;
 float at3 = 0.0;
 float qd = 0;
@@ -103,12 +95,6 @@ float sintheta1=0;
 float sintheta32 = 0;
 float costheta32 = 0;
 
-//float kp1f = 300;
-//float kp2f = 300;
-//float kp3f = 160;
-//float kd1f = 1.6;
-//float kd2f = 1.9;
-//float kd3f = 1.9;
 
 float Theta1_old = 0;
 float Omega1_old1 = 0;
@@ -126,10 +112,12 @@ float x = 0;
 float y = 0;
 float z = 0;
 
+//proportion control effort in base frame
 float KPx = 500.0;
 float KPy = 500.0;
 float KPz = 500.0;
 
+//derivative control effort in base frame
 float KDx = 10.0;
 float KDy = 10.0;
 float KDz = 10.0;
@@ -155,6 +143,7 @@ float z_old = 0;
 float z_dot_old = 0;
 float z_dot_old2 = 0;
 
+//desired location
 float xd = 0;
 float yd = 0;
 float zd = 0;
@@ -167,13 +156,14 @@ float JF1 = 0.0;
 float JF2 = 0.0;
 float JF3 = 0.0;
 
-float ff = 0.8;
+float ff = 0.8; //friction multiplier
 float Kt = 6.0;
 float FZcmd = 12.0; // Gravity comp constant
 float JFZ1 = 0.0;
 float JFZ2 = 0.0;
 float JFZ3 = 0.0;
 
+//rotation matrix
 float R11 = 0;
 float R12 = 0;
 float R13 = 0;
@@ -219,55 +209,37 @@ float xdw = 0;
 float ydw = 0;
 float zdw = 0;
 
+//proportion control effort in n frame
 float KPxn = 500.0;
 float KPyn = 500.0;
 float KPzn = 500.0;
 
+//derivative control effort in n frame
 float KDxn = 50.0;
 float KDyn = 50.0;
 float KDzn = 50.0;
 
+//straight line trajectory distance
 float dirx = 0.0;
 float diry = 0.0;
 float dirz = 0.0;
 
+//point a of straight line trajectory
 float xa = 0.2;
 float ya = 0.1;
 float za = 0.3;
 
+//point b of straight line trajectory
 float xb = 0.4;
 float yb = -0.1;
 float zb = 0.3;
 
-float speed = 0.15;
+float speed = 0.15; //movespeed
 float t_start = 0.0;
 float t_total = 0.0;
 int state = 0; //0 is heading to b, 1 is heading to a
 
-typedef struct steptraj_s {
-    long double b[5];
-    long double a[5];
-    long double xk[5];
-    long double yk[5];
-    float qd_old;
-    float qddot_old;
-    int size;
-} steptraj_t;
 
-steptraj_t trajectory = {4.7698076658265394e-08L,1.9079230663306158e-07L,2.8618845994959235e-07L,1.9079230663306158e-07L,4.7698076658265394e-08L,
-                        1.0000000000000000e+00L,-3.8817733990147785e+00L,5.6505617704870286e+00L,-3.6557000616943993e+00L,8.8691245339137526e-01L,
-                        0,0,0,0,0,
-                        0,0,0,0,0,
-                        0,
-                        0,
-                        5};
-steptraj_t trajectory2 = {4.7698076658265394e-08L,1.9079230663306158e-07L,2.8618845994959235e-07L,1.9079230663306158e-07L,4.7698076658265394e-08L,
-                        1.0000000000000000e+00L,-3.8817733990147785e+00L,5.6505617704870286e+00L,-3.6557000616943993e+00L,8.8691245339137526e-01L,
-                        0,0,0,0,0,
-                        0,0,0,0,0,
-                        0,
-                        0,
-                        5};
 // this function must be called every 1ms.
 void implement_discrete_tf(steptraj_t *traj, float step, float *qd, float *qd_dot, float *qd_ddot) {
     int i = 0;
@@ -321,24 +293,6 @@ void lab(float theta1motor,float theta2motor,float theta3motor,float *tau1,float
     printtheta2motor = theta2motor;
     printtheta3motor = theta3motor;
 
-//    x = (127.0*cos(theta1motor)*(cos(theta3motor) + sin(theta2motor)))/500.0;
-//    y = (127.0*sin(theta1motor)*(cos(theta3motor) + sin(theta2motor)))/500.0;
-//    z = (127.0*cos(theta2motor))/500.0 - (127.0*sin(theta3motor))/500.0 + 127.0/500.0;
-    //t = (mycount%(6283*4))*0.001;
-//    t = mycount*0.001;
-
-//    x = 0.3;
-
-    // Calculated motor thetas with inverse kinematics
-//    motortheta1 = atan2(y,x);
-//    motortheta2 = 1.570796 - 1.0*atan2(1.0*z - 0.254, sqrt(x*x + y*y)) - 1.0*atan2(2.0*sqrt(0.064516 - 0.25*x*x - 0.25*y*y - 0.25*(z - 0.254)*(z-0.254)), sqrt((z - 0.254)*(z - 0.254) + x*x + y*y));
-//    motortheta3 = 1.0*atan2(2.0*sqrt(0.064516 - 0.25*x*x - 0.25*y*y - 0.25*(z - 0.254)*(z-0.254)), sqrt((z - 0.254)*(z-0.254) + x*x + y*y)) - 1.0*atan2(1.0*z - 0.254, sqrt(x*x + y*y));
-
-    // DH Thetas
-//    theta1 = motortheta1;
-//    theta2 = motortheta2 - PI/2;
-//    theta3 = motortheta3 - motortheta2 + PI/2;
-
 
     Omega1 = (theta1motor - Theta1_old)/0.001;
     Omega1 = (Omega1 + Omega1_old1 + Omega1_old2)/3.0;
@@ -360,47 +314,6 @@ void lab(float theta1motor,float theta2motor,float theta3motor,float *tau1,float
     //order matters here. Because we are saving the old value first before overriding it
     Omega3_old2 = Omega3_old1;
     Omega3_old1 = Omega3;
-
-
-//    t = (mycount%2000)*0.001;
-////    new trajectory
-//    if((mycount%4000)==0) {
-//        if(step1 > 0.26) {
-//            step1 = 0.25;
-//        } else {
-//            step1 = 0.85;
-//        }
-//    }
-//    if((mycount%4000)==0) {
-//        if(step2 < 0.29) {
-//            step2 = 0.3;
-//        } else {
-//            step2 = -0.3;
-//        }
-//    }
-
-
-//    implement_discrete_tf(&trajectory, step1, &qd, &qddot, &qdddot);
-//    theta1d = qd;
-//    thetadot = qddot;
-//    thetaddot = qdddot;
-//    implement_discrete_tf(&trajectory2, step2, &qd, &qddot, &qdddot);
-//    theta3d = qd;
-//    theta3dot = qddot;
-//    theta3ddot = qdddot;
-//
-//
-//    err1 = theta1d-theta1motor;
-//    err2 = theta1d-theta2motor;
-//    err3 = theta3d-theta3motor;
-//
-//    err_dot1 = thetadot - Omega1;
-//    err_dot2 = thetadot - Omega2;
-//    err_dot3 = theta3dot - Omega3;
-//
-//    Ik1 = Ikold1+(err1-err_old1)/2.0*0.001;
-//    Ik2 = Ikold2+(err2-err_old2)/2.0*0.001;
-//    Ik3 = Ikold3+(err3-err_old3)/2.0*0.001;
 
 
     //friction
@@ -428,7 +341,7 @@ void lab(float theta1motor,float theta2motor,float theta3motor,float *tau1,float
      u_fric3 = v_co_3*Omega3;
     }
 
-    //rotation to new frame
+    //rotation matrix
     cosz = cos(thetaz);
     sinz = sin(thetaz);
     cosx = cos(thetax);
@@ -447,33 +360,31 @@ void lab(float theta1motor,float theta2motor,float theta3motor,float *tau1,float
 
 
 //    //Task Space PD Control
-//    xd = 0.254;
+//    xd = 0.254; //desired worldframe coordinate
 //    yd = 0.254;
 //    zd = 0.254;
 //
-//
+//    //calculating acutal world frame coordinates
 //    x = (127.0*cos(theta1motor)*(cos(theta3motor) + sin(theta2motor)))/500.0;
 //    y = (127.0*sin(theta1motor)*(cos(theta3motor) + sin(theta2motor)))/500.0;
 //    z = (127.0*cos(theta2motor))/500.0 - (127.0*sin(theta3motor))/500.0 + 127.0/500.0;
 //
+//    //calculating speed
 //    x_dot = (x - x_old)/0.001;
 //    x_dot = (x_dot + x_dot_old + x_dot_old2)/3.0;
 //    x_old = x;
-//    //order matters here. Because we are saving the old value first before overriding it
 //    x_dot_old2 = x_dot_old;
 //    x_dot_old = x_dot;
 //
 //    y_dot = (y - y_old)/0.001;
 //    y_dot = (y_dot + y_dot_old + y_dot_old2)/3.0;
 //    y_old = y;
-//    //order matters here. Because we are saving the old value first before overriding it
 //    y_dot_old2 = y_dot_old;
 //    y_dot_old = y_dot;
 //
 //    z_dot = (z - z_old)/0.001;
 //    z_dot = (z_dot + z_dot_old + z_dot_old2)/3.0;
 //    z_old = z;
-//    //order matters here. Because we are saving the old value first before overriding it
 //    z_dot_old2 = z_dot_old;
 //    z_dot_old = z_dot;
 //
@@ -481,6 +392,7 @@ void lab(float theta1motor,float theta2motor,float theta3motor,float *tau1,float
 //    Fy = KPy*(yd-y) + KDy*(yd_dot - y_dot);
 //    Fz = KPz*(zd-z) + KDz*(zd_dot - z_dot);
 //
+//    //Jacobian multiply by the Fxyz
 //    JF1 = (-0.254*sin(theta1motor)*(cos(theta3motor)+sin(theta2motor)))*Fx
 //            + (0.254*cos(theta1motor)*(cos(theta3motor)+sin(theta2motor)))*Fy + 0*Fz;
 //    JF2 = (0.254*cos(theta1motor)*(cos(theta2motor)-sin(theta3motor)))*Fx
@@ -489,14 +401,20 @@ void lab(float theta1motor,float theta2motor,float theta3motor,float *tau1,float
 //    JF3 = - 0.254*sin(theta3motor)*cos(theta1motor)*Fx - 0.254*sin(theta1motor)*sin(theta3motor)*Fy
 //            - 0.254*cos(theta3motor)*Fz;
 
-
+//    //Zcmd force
 //    JFZ1 = 0*FZcmd/Kt;
 //    JFZ2 = -0.254*(cos(theta3motor)+sin(theta2motor))*FZcmd/Kt;
 //    JFZ3 = -0.254*cos(theta3motor)*FZcmd/Kt;
 
 
-    // Straight Line Trajectory
+    /* straight line trajectory
+    t is calculated in seconds
+    The first if statement switch between two states when one end of point is reached, 
+    state zero goes from point a to point b
+    state one goes from point b to point a
+    */
     t = mycount * 0.001;
+    // Starts a new line when reaching the goal
     if((t-t_start) > t_total) {
         if(state == 0) { // We got to point b
             state = 1; // Now heading to a
@@ -505,19 +423,29 @@ void lab(float theta1motor,float theta2motor,float theta3motor,float *tau1,float
         }
         t_start = t;
     }
+    // point a to point b
     if (state == 0) {
+        //distance from b to a
         dirx = xb - xa;
         diry = yb - ya;
         dirz = zb - za;
+        //total time it would take to travel from two points.
+        //We use distance formula to find the distance and divide it by desired speed to find the total time
         t_total = sqrt(dirx*dirx + diry*diry + dirz*dirz)/speed;
+        //current desired location, multiply by the average speed in each direction mulitply by the amount of time since state switch, and add the result to point a
         xdw = dirx * (t-t_start) / t_total + xa;
         ydw = diry * (t-t_start) / t_total + ya;
         zdw = dirz * (t-t_start) / t_total + za;
+    // point b to point a
     } else if (state == 1) {
+        //distance from a to b
         dirx = xa - xb;
         diry = ya - yb;
         dirz = za - zb;
+        //total time it would take to travel from two points.
+        //We use distance formula to find the distance and divide it by desired speed to find the total time
         t_total = sqrt(dirx*dirx + diry*diry + dirz*dirz)/speed;
+        //current desired location, multiply by the average speed in each direction mulitply by the amount of time since state switch, and add the result to point b
         xdw = dirx * (t-t_start) / t_total + xb;
         ydw = diry * (t-t_start) / t_total + yb;
         zdw = dirz * (t-t_start) / t_total + zb;
@@ -525,42 +453,38 @@ void lab(float theta1motor,float theta2motor,float theta3motor,float *tau1,float
 
 
     //Task Space PD Control rotation
-//    xdw = 0.254;
-//    ydw = 0.254;
-//    zdw = 0.254;
+    // desired world coordinates multiply by rotation matrix
     xd = RT11*xdw+RT12*ydw+RT13*zdw;
     yd = RT21*xdw+RT22*ydw+RT23*zdw;
     zd = RT31*xdw+RT32*ydw+RT33*zdw;
-
+    //calculating acutal world coordinates
     xw = (127.0*cos(theta1motor)*(cos(theta3motor) + sin(theta2motor)))/500.0;
     yw= (127.0*sin(theta1motor)*(cos(theta3motor) + sin(theta2motor)))/500.0;
     zw = (127.0*cos(theta2motor))/500.0 - (127.0*sin(theta3motor))/500.0 + 127.0/500.0;
 
+    //acutal world coordinates multiply by rotation matrix
     x = RT11*xw+RT12*yw+RT13*zw;
     y = RT21*xw+RT22*yw+RT23*zw;
     z = RT31*xw+RT32*yw+RT33*zw;
-
+    //calculating speed
     x_dot = (x - x_old)/0.001;
     x_dot = (x_dot + x_dot_old + x_dot_old2)/3.0;
     x_old = x;
-    //order matters here. Because we are saving the old value first before overriding it
     x_dot_old2 = x_dot_old;
     x_dot_old = x_dot;
 
     y_dot = (y - y_old)/0.001;
     y_dot = (y_dot + y_dot_old + y_dot_old2)/3.0;
     y_old = y;
-    //order matters here. Because we are saving the old value first before overriding it
     y_dot_old2 = y_dot_old;
     y_dot_old = y_dot;
 
     z_dot = (z - z_old)/0.001;
     z_dot = (z_dot + z_dot_old + z_dot_old2)/3.0;
     z_old = z;
-    //order matters here. Because we are saving the old value first before overriding it
     z_dot_old2 = z_dot_old;
     z_dot_old = z_dot;
-
+    //Controll Effort
     Fx = KPxn*(xd-x) + KDxn*(xd_dot - x_dot);
     Fy = KPyn*(yd-y) + KDyn*(yd_dot - y_dot);
     Fz = KPzn*(zd-z) + KDzn*(zd_dot - z_dot);
@@ -581,37 +505,6 @@ void lab(float theta1motor,float theta2motor,float theta3motor,float *tau1,float
     *tau2 = JF2 + ff*u_fric2;
     *tau3 = JF3 + ff*u_fric3;
 
-//    new traj
-//    if (switch_control == 1) {
-//        //inverse dynamic
-//        ptau1 = kp1*(err1) + kd1*err_dot1+thetaddot*J1;
-//
-//        at2 = kp2*(err2) + kd2*err_dot2+thetaddot;
-//
-//        at3 = kp3*(err3) + kd3*err_dot3+theta3ddot;
-//
-//        sintheta32 = sin(theta3motor-theta2motor);
-//        costheta32 = cos(theta3motor-theta2motor);
-//        ptau2 = p1*at2-(p3*sintheta32)*at3 - p3*costheta32*Omega3*Omega3 - p4*9.8*sin(theta2motor);
-//        ptau3 = -p3*sintheta32*at2+p2*at3 - p3*costheta32*Omega2*Omega2 - p5*9.8*sin(theta3motor);
-//    } else {
-//        // feed forward
-//        ptau1 = kp1f*(err1) + kd1f*err_dot1+thetaddot*J1;
-//        ptau2 = kp2f*(err2) + kd2f*err_dot2+thetaddot*J2;
-//        ptau3 = kp3f*(err3) + kd3f*err_dot3+theta3ddot*J3;
-//    }
-
-//    *tau1 = ptau1;
-//    *tau2 = ptau2;
-//    *tau3 = ptau3;
-
-//    Ikold1 = Ik1;
-//    Ikold2 = Ik2;
-//    Ikold3 = Ik3;
-//
-//    err_old1 = err1;
-//    err_old2 = err2;
-//    err_old3 = err3;
 
     // When we want to plot error
     Simulink_PlotVar1 = xd-x;
